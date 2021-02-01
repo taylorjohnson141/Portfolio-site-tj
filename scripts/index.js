9268ef82d01da604b02a8c64cb5d1512623782f9
Taylor Johnson <taylorjohnson141@gmail.com>
2021-01-31 21:34:15 -0700

    Add click events for all navEvents

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 let portfolioLink = document.querySelector('.portfolioLink')
+let resumeLink = document.querySelector('.resumeLink')
+let aboutLink = document.querySelector('.aboutLink')
+let connectLink = document.querySelector('.connectLink')
+
 let portfolioPage = document.querySelector('.portfolio')
-let displayPortfolio = () =>{
-  console.log('o')
-  portfolioPage.classList.remove('hidden')
+let resumePage = document.querySelector('.resume')
+let aboutPage = document.querySelector('.about')
+let connectPage = document.querySelector('.connect')
+
+let displayPage = (element) =>{
+  element.classList.remove('hidden')
 }
 
-portfolioLink.addEventListener('click', displayPortfolio);
+portfolioLink.addEventListener('click', ()=>{
+  displayPage(portfolioPage)
+});
+resumeLink.addEventListener('click', () =>{
+  displayPage(resumePage)
+});
+aboutLink.addEventListener('click', () =>{
+  displayPage(aboutPage)
+});
+connectLink.addEventListener('click', () =>{
+  displayPage(connectPage)
+});
 
